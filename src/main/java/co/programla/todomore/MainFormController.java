a4b85b4039949cf0d5a2a72456cabb91cdb18d80
kenan.erarslan <kenan.erarslan@NCEL30537.iis.amadeus.net>
2016-03-29 17:52:17 +0200

    -Alert message after time is up. -Logging of operations (to be continue)

--- src/main/java/co/programla/todomore/MainFormController.java
+++ src/main/java/co/programla/todomore/MainFormController.java
@@ -1,17 +1,22 @@
 package co.programla.todomore;
 
 import java.net.URL;
+import java.text.MessageFormat;
+import static java.text.MessageFormat.format;
 import java.util.ResourceBundle;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressIndicator;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 /**
@@ -19,7 +24,7 @@ import javafx.util.Duration;
  *
  * @author Kenan ERARSLAN <kenan at enginaar.com>
  */
-public class MainFormController implements Initializable {
+public class MainFormController implements Initializable, EventHandler<ActionEvent> {
 
     private Timeline timeline;
     @FXML
@@ -28,29 +33,27 @@ public class MainFormController implements Initializable {
     private ProgressIndicator prgTimer;
     @FXML
     private Button btnCount;
+    @FXML
+    private VBox cntTimeline;
 
     private int countdown;
     private State state;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        state = State.INITIAL;
-        reset();        
+        reset(State.INITIAL);
+        timeline = new Timeline(new KeyFrame(Duration.ZERO, new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                countdown--;
+                refreshUI();
+            }
+        }), new KeyFrame(Duration.seconds(1)));
+        timeline.setOnFinished(this);
+        timeline.setCycleCount(countdown);
     }
 
     public void count() {
-        if (timeline == null) {
-            timeline = new Timeline(new KeyFrame(Duration.ZERO, new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent actionEvent) {
-                    countdown--;
-                    refreshUI();
-                }
-            }), new KeyFrame(Duration.seconds(1)));
-            timeline.setCycleCount(countdown);
-
-        }
-
         Animation.Status status = timeline.getStatus();
         switch (status) {
             case PAUSED:
@@ -68,35 +71,54 @@ public class MainFormController implements Initializable {
     private void refreshUI() {
         lblCounter.setText(countdown / 60 + ":" + countdown % 60);
         prgTimer.setProgress((double) (state.timeAsSecond() - countdown) / state.timeAsSecond());
+        timeline.setCycleCount(countdown);
     }
 
-    public void reset() {
+    public void reset(State stt) {
+        cntTimeline.getChildren().add(new Label(MessageFormat.format("{0} state changed to {1}", state, stt)));
+        state = stt;
         countdown = state.timeAsSecond();
+    }
+
+    public void rst() {
+        reset(state);
         refreshUI();
     }
-    
+
     public void shortBreak() {
-        state = State.SHORT_BREAK;
-        reset();
+        reset(State.SHORT_BREAK);
+        refreshUI();
     }
-    
+
     public void longBreak() {
-        state = State.LONG_BREAK;
-        reset();
+        reset(State.LONG_BREAK);
+        refreshUI();
     }
-   
+
     public void workRegular() {
-        state = State.WORKING;
-        reset();
+        reset(State.WORKING);
+        refreshUI();
     }
-    
+
     public void workRandomly() {
-        state = State.JUST_WORK;
-        reset();
+        reset(State.JUST_WORK);
+        refreshUI();
     }
-    
+
     public void breakRandomly() {
-        state = State.JUST_BREAK;
-        reset();
+        reset(State.JUST_BREAK);
+        refreshUI();
+    }
+
+    public void handle(ActionEvent event) {
+        Platform.runLater(new Runnable() {
+            public void run() {
+                Alert a = new Alert(Alert.AlertType.ERROR);
+                a.setTitle("Time is up");
+                a.setContentText(format("Your time for {1} is up, please go on next step", state));
+                a.showAndWait();
+            }
+        });
+
     }
 }
